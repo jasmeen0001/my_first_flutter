00b56912ae6b9b29376fba61e66223b999856439
Jasmeen Kaur <jasmeenkaursohi@Jasmeens-MacBook-Air.local>
2024-02-14 15:00:07 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -9,46 +11,89 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: WeatherApp(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class WeatherApp extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _WeatherAppState createState() => _WeatherAppState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _WeatherAppState extends State<WeatherApp> {
   TextEditingController gatewayIdController = TextEditingController();
   TextEditingController nodeIdController = TextEditingController();
-  DateTime? startDate;
-  DateTime? endDate;
+  TextEditingController startDateController = TextEditingController();
+  TextEditingController endDateController = TextEditingController();
+
+  DateTime? selectedStartDate = DateTime.now();
+  DateTime? selectedEndDate = DateTime.now();
 
   Future<void> _selectDate(BuildContext context, bool isStartDate) async {
-    DateTime? selectedDate = await showDatePicker(
+    DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: DateTime.now(),
+      initialDate: isStartDate ? selectedStartDate ?? DateTime.now() : selectedEndDate ?? DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2101),
     );
 
-    if (selectedDate != null) {
+    if (picked != null) {
       setState(() {
         if (isStartDate) {
-          startDate = selectedDate;
+          selectedStartDate = picked;
+          startDateController.text = "${picked.toLocal()}".split(' ')[0];
         } else {
-          endDate = selectedDate;
+          selectedEndDate = picked;
+          endDateController.text = "${picked.toLocal()}".split(' ')[0];
         }
       });
     }
   }
 
-  void _onSubmitPressed() {
+  Future<void> _selectTime(BuildContext context, bool isStartDate) async {
+    TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: isStartDate
+          ? TimeOfDay.fromDateTime(selectedStartDate ?? DateTime.now())
+          : TimeOfDay.fromDateTime(selectedEndDate ?? DateTime.now()),
+    );
+
+    if (picked != null) {
+      setState(() {
+        if (isStartDate) {
+          selectedStartDate = DateTime(
+            selectedStartDate!.year,
+            selectedStartDate!.month,
+            selectedStartDate!.day,
+            picked.hour,
+            picked.minute,
+          );
+          startDateController.text = "${selectedStartDate!.toLocal()}".split(' ')[1];
+        } else {
+          selectedEndDate = DateTime(
+            selectedEndDate!.year,
+            selectedEndDate!.month,
+            selectedEndDate!.day,
+            picked.hour,
+            picked.minute,
+          );
+          endDateController.text = "${selectedEndDate!.toLocal()}".split(' ')[1];
+        }
+      });
+    }
+  }
+
+  void _navigateToFetchedData() {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => FetchedDataPage(),
+        builder: (context) => FetchedData(
+          gatewayId: gatewayIdController.text,
+          nodeId: nodeIdController.text,
+          startDate: selectedStartDate!,
+          endDate: selectedEndDate!,
+        ),
       ),
     );
   }
@@ -57,12 +102,12 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Date Selection App'),
+        title: Text('Weather App'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
               controller: gatewayIdController,
@@ -72,47 +117,21 @@ class _HomePageState extends State<HomePage> {
               controller: nodeIdController,
               decoration: InputDecoration(labelText: 'Node ID'),
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    readOnly: true,
-                    controller: TextEditingController(
-                      text: startDate != null
-                          ? "${startDate!.toLocal()}".split(' ')[0]
-                          : '',
-                    ),
-                    decoration: InputDecoration(labelText: 'Start Date'),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () => _selectDate(context, true),
-                  icon: Icon(Icons.date_range),
-                ),
-              ],
+            TextField(
+              controller: startDateController,
+              readOnly: true,
+              onTap: () => _selectDate(context, true),
+              decoration: InputDecoration(labelText: 'Start Date'),
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    readOnly: true,
-                    controller: TextEditingController(
-                      text: endDate != null
-                          ? "${endDate!.toLocal()}".split(' ')[0]
-                          : '',
-                    ),
-                    decoration: InputDecoration(labelText: 'End Date'),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () => _selectDate(context, false),
-                  icon: Icon(Icons.date_range),
-                ),
-              ],
+            TextField(
+              controller: endDateController,
+              readOnly: true,
+              onTap: () => _selectDate(context, false),
+              decoration: InputDecoration(labelText: 'End Date'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _onSubmitPressed,
+              onPressed: () => _navigateToFetchedData(),
               child: Text('Submit'),
             ),
           ],
@@ -122,12 +141,44 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class FetchedDataPage extends StatelessWidget {
-  // Example data, replace with your actual data
-  final double temperature = 25.5;
-  final double humidity = 60.0;
-  final double carbonDioxide = 400.0;
-  final String timestamp = DateTime.now().toString();
+class FetchedData extends StatefulWidget {
+  final String gatewayId;
+  final String nodeId;
+  final DateTime startDate;
+  final DateTime endDate;
+
+  FetchedData({
+    required this.gatewayId,
+    required this.nodeId,
+    required this.startDate,
+    required this.endDate,
+  });
+
+  @override
+  _FetchedDataState createState() => _FetchedDataState();
+}
+
+class _FetchedDataState extends State<FetchedData> {
+  List<Map<String, dynamic>> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(
+        Uri.parse('https://qqvlf6v6kc.execute-api.us-east-1.amazonaws.com/v1/data?nodeId=${widget.nodeId}&gatewayId=${widget.gatewayId}&starttime=${widget.startDate.millisecondsSinceEpoch ~/ 1000}&endtime=${widget.endDate.millisecondsSinceEpoch ~/ 1000}'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        data = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -136,17 +187,27 @@ class FetchedDataPage extends StatelessWidget {
         title: Text('Fetched Data'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Card(
+          elevation: 5,
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+            padding: EdgeInsets.all(16.0),
+            child: Row(
               children: [
-                Text('Temperature: $temperature'),
-                Text('Humidity: $humidity'),
-                Text('Carbon Dioxide: $carbonDioxide'),
-                Text('Timestamp: $timestamp'),
+                Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Temperature: ${data.isNotEmpty ? data[0]['temperature'] : 'N/A'}'),
+                        Text('Humidity: ${data.isNotEmpty ? data[0]['humidity'] : 'N/A'}'),
+                        Text('CO2: ${data.isNotEmpty ? data[0]['co2'] : 'N/A'}'),
+                        Text('Timestamp: ${data.isNotEmpty ? data[0]['timestamp'] : 'N/A'}'),
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
